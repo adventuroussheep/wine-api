8d62884bb4633651482e9fafb73413ff974729d9
Jacob Haas <jacob_haas_7@hotmail.com>
2020-03-26 17:27:06 -0700

    Updates

---
+++ precache-manifest.3cc8671dfe65b9a8ac79e210fc379bfe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb4e640285210e5f096b8cf7302639d",
+    "url": "/wine-api/index.html"
+  },
+  {
+    "revision": "642db153d11470bcf516",
+    "url": "/wine-api/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "06e9b711482fffc09c9d",
+    "url": "/wine-api/static/css/main.18a85abf.chunk.css"
+  },
+  {
+    "revision": "642db153d11470bcf516",
+    "url": "/wine-api/static/js/2.ce9ee6e7.chunk.js"
+  },
+  {
+    "revision": "2ade8cbc5191dce489b21485a18d88c5",
+    "url": "/wine-api/static/js/2.ce9ee6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e9b711482fffc09c9d",
+    "url": "/wine-api/static/js/main.d310b5f6.chunk.js"
+  },
+  {
+    "revision": "4cd8ec3d0215525334fa",
+    "url": "/wine-api/static/js/runtime-main.785c93bb.js"
+  },
+  {
+    "revision": "3af0bc2d343e5d8e7b5b8edae1a3c3e7",
+    "url": "/wine-api/static/media/paper.3af0bc2d.jpg"
+  },
+  {
+    "revision": "333ab378163ebf29dacfa9f3949ab9c2",
+    "url": "/wine-api/static/media/parchment.333ab378.jpg"
+  },
+  {
+    "revision": "7d28866f14a2b2ff79adb483e18114ef",
+    "url": "/wine-api/static/media/wineTable.7d28866f.jpg"
+  }
+]);
